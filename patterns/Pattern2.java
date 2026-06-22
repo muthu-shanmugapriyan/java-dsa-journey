85947ce15493f4e5d9cad837f01d30dbc56b87af
muthu-shanmugapriyan <muthu.shanmugapriyan@gmail.com>
2026-06-22 12:59:50 +0530

    Pattern question

---
+++ patterns/Pattern2.java
@@ -0,0 +1,15 @@
+package com.shannu;
+
+public class Pattern2 {
+    static void main(String[] args) {
+            pattern2(5);
+    }
+    static void pattern2(int n){
+        for (int row=1;row<=n;row++){
+            for(int col=1;col<=n;col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
