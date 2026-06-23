17a047f5784ede8b983faed387a56cfbb0cbe1b9
muthu-shanmugapriyan <muthu.shanmugapriyan@gmail.com>
2026-06-23 09:08:59 +0530

    Pattern Problems

---
+++ patterns/Pattern3.java
@@ -0,0 +1,16 @@
+package com.shannu;
+
+public class Pattern3 {
+    static void main(String[] args) {
+        pattern(5);
+    }
+    static void pattern(int n){
+        for (int row=1;row<=n;row++){
+            for(int col=1;col<=n-row+1;col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
