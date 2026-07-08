16948de62a08ec32a076c96d02e0a9114c32662c
muthu-shanmugapriyan <muthu.shanmugapriyan@gmail.com>
2026-07-08 09:27:55 +0530

    Recursion

---
+++ Recursion/SkipWord.java
@@ -0,0 +1,19 @@
+package com.string;
+
+public class SkipWord {
+    static void main(String[] args) {
+        System.out.println(  skip("abdbjapplebjb"));
+    }
+    static String skip(String up){
+        if(up.isEmpty()){
+            return "";
+        }
+        char ch=up.charAt(0);
+        if(up.startsWith("apple")){
+            return skip(up.substring(5));
+        }
+        else{
+          return up.charAt(0)+skip(up.substring(1));
+        }
+    }
+}
