29d4a6eec516e82632232b762c9f42cdbd0a9448
muthu-shanmugapriyan <muthu.shanmugapriyan@gmail.com>
2026-07-22 19:20:26 +0530

    Recursion

---
+++ Recursion/shannu/FibonacciUsingRecursion.java
@@ -0,0 +1,13 @@
+package com.shannu;
+
+public class FibonacciUsingRecursion {
+    static void main(String[] args) {
+        System.out.println(fibo(6));
+    }
+    static int fibo(int n){
+        if(n<2){
+            return n;
+        }
+        return fibo(n-1)+fibo(n-2);
+    }
+}
---
+++ Recursion/shannu/Main.java
@@ -0,0 +1,11 @@
+package com.shannu;
+public class Main {
+    static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            message();
+        }
+    }
+    static void message(){
+        System.out.println("Hello");
+    }
+}
